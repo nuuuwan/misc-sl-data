53dd7c3de9f9af67f80c54d362790b59d1687690
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2072.local>
2020-08-29 09:09:18 +0530

    Daily Upload 2020-08-29 09:08 by curl_and_upload.sh

--- curl_and_upload.sh
+++ curl_and_upload.sh
@@ -1,5 +1,7 @@
 curl https://s3.eu-central-1.amazonaws.com/hdx-ckan-filestore-prod/resources/3a3d399c-85e1-4872-b610-3245b7544de4/conflict_data_lka.csv > conflict_data_lka.csv
 
+curl https://s3.eu-central-1.amazonaws.com/hdx-ckan-filestore-prod/resources/3638f0d6-9969-48cf-a919-1d879d037ec6/wfp_food_prices_sri-lanka.csv > food.hdx.wfp_food_prices_sri-lanka.csv 
+
 git add *
 git commit -a -m "Daily Upload $(date '+%Y-%m-%d %H:%m') by curl_and_upload.sh"
 git push origin master
